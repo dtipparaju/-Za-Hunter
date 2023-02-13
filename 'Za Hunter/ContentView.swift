7a266cfbe244b8af5de4064fc0bc1517262d40fc
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-02-13 09:55:58 -0600

    Added Map Search Feature

--- 'Za Hunter/ContentView.swift
+++ 'Za Hunter/ContentView.swift
@@ -12,14 +12,37 @@ struct ContentView: View {
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 42.15704, longitude: -88.14812), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     @StateObject var locationManager = LocationManager()
     @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var places = [Place]()
     var body: some View {
         VStack {
             Map(
                 coordinateRegion: $region,
                 interactionModes: .all,
                 showsUserLocation: true,
-                userTrackingMode: $userTrackingMode
-            )
+                userTrackingMode: $userTrackingMode,
+                annotationItems: places) { places in
+                    MapMarker(coordinate: places.annotation.coordinate)
+                }
+                .onAppear(perform: {
+                    performSerach(item: "Pizza")
+                })
+        }
+    }
+    func performSerach(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
+        let search = MKLocalSearch(request: searchRequest)
+        
+        search.start { (response, error) in
+            if let response = response {
+                for mapItem in response.mapItems {
+                 let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    places.append(Place(annotation: annotation, mapItem: mapItem))
+                }
+            }
         }
     }
 }
@@ -28,4 +51,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Place: Identifiable {
+    let id = UUID()
+    let annotation: MKPointAnnotation
+    let mapItem: MKMapItem
 }
