796978100e05ae37955c8803515cc1354eb10eb6
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-02-13 09:33:07 -0600

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Dhanush Tipparaju on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
